33f821a25b3ed71d82ee062830cc611af296dcf3
Prime0007 <Agedeoluwaseun31@gmail.com>
2022-10-14 17:32:58 -0700

    task 2 done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,14 +4,14 @@
 /**
  *main - Entry
  *Return: 0
- *     */
+ */
 int main(void)
 {
 	int n;
 	int ldigit;
 
 	srand(time(0));
-	n = rand() - RAND_MAX */ 2;
+	n = rand() - RAND_MAX / 2;
 	ldigit = n % 10;
 	if (ldigit > 5)
 	{
