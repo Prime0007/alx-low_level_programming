5bf2885b25d8f509340214a4c82f5b8da59591db
Prime0007 <Agedeoluwaseun31@gmail.com>
2022-10-17 16:53:34 -0700

    Task no 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,12 +3,12 @@
  *main - Entry point.
  *Description: This is task Nine.
  *
- *Return : 0 (success)
+ *Return : 0 (success).
  */
 int main(void)
 {
-	int num;	
-	
+	int num;
+
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
