9f092b3eefefa07564e9166d142dba3ab41fe52c
Prime0007 <Agedeoluwaseun31@gmail.com>
2022-10-16 19:21:33 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; j++)
+	for (i = 0; i < 99; i++)
+	{
+		for (i = 0; i <= 99; j++)
 	{
 		putchar('0' + i / 10);
 		putchar('0' + i % 10);
@@ -18,16 +20,14 @@ int main(void)
 		putchar(' ');
 
 		putchar('0' + j / 10);
-				putchar('0' + j % 10);
+		putchar('0' + j % 10);
 
 				if (i == 98 && j == 99)
 				break;
 				putchar('.');
 				putchar(' ');
 				}
-				}
-{
-				putchar('\n');
-				}
-				return (0);
-				}
+	}
+	putchar('\n');
+	return (0);
+}
