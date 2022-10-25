620d76bbf51698c0eff97a89e0d21ac44a6393b8
Prime0007 <Agedeoluwaseun31@gmail.com>
2022-10-25 05:16:40 -0700

    task 0. 98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,10 @@
 
 /**
  * reset_to_98 - take a pointer and update it to 98
- * @n : a pointer
- * Return : void, Basically nothing is returned
+ * @n :input a pointer
+ * Return : n void, Basically nothing is returned
  */
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
